be66b044ec81ae799517caa94ba23181601d5f61
Woodhouse3D <45299674+FloridaChristopher@users.noreply.github.com>
2024-04-24 19:47:14 -0400

    changed vga port to JB-JC
    - this makes more room for other attachments

--- Zybo-Z7-20-Pmod-VGA.runs/impl_1/top.tcl
+++ Zybo-Z7-20-Pmod-VGA.runs/impl_1/top.tcl
@@ -123,7 +123,6 @@ set ACTIVE_STEP init_design
 set rc [catch {
   create_msg_db init_design.pb
   set_param chipscope.maxJobs 3
-  set_param xicom.use_bs_reader 1
 OPTRACE "create in-memory project" START { }
   create_project -in_memory -part xc7z020clg400-1
   set_property board_part_repo_paths {E:/Github/Zybo7Z20_VGA_PONG/Zybo-Z7-20-Pmod-VGA.board} [current_project]
